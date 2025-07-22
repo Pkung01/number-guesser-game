e8e1d3e6aac550e53743ad6abe53686fe2ddca40
MIX2546 <pichaiyuth27725466@gmail.com>
2025-07-22 16:51:14 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,8 @@
 def check_guess(secret_number, player_guess):
     """
     Task for Student 2:
-    1. Compare the player's guess with the secret number.
-    2. If the guess is correct, return the string "correct".
-    3. If the guess is too high, return the string "high".
-    4. If the guess is too low, return the string "low".
+    Compares the player's guess to the secret number.
+    Returns "correct", "high", or "low".
     """
     if player_guess == secret_number:
         return "correct"
