3b52698798d7edbcd31e84abe44b2688f6b4e3bb
MIX2546 <pichaiyuth27725466@gmail.com>
2025-07-22 16:12:24 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
+def check_guess(secret_number, player_guess):
+    if player_guess == secret_number:
+        return "correct"
+    elif player_guess > secret_number:
+        return "high"
+    else:
+        return "low"
 
